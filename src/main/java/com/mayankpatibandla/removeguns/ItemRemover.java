0da6e47af18fd018fb51af6d3282ee03ea12b00b
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-07-09 03:23:40 -0400

    Config file for item removal

--- src/main/java/com/mayankpatibandla/removeguns/ItemRemover.java
+++ src/main/java/com/mayankpatibandla/removeguns/ItemRemover.java
@@ -1,27 +1,35 @@
 package com.mayankpatibandla.removeguns;
 
+import com.mayankpatibandla.removeguns.config.ConfigurationHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
+import net.minecraftforge.common.config.Property;
 
 public class ItemRemover {
     @SubscribeEvent
     public void removeFromInventory(TickEvent.PlayerTickEvent event) {
         // Remove guns from the player's inventory
+        Property items_list = ConfigurationHandler.getItems();
         EntityPlayer player = event.player;
 
-        for(Object x : player.inventoryContainer.getInventory()) {
-            // Remove guns from the player's inventory
-            if(x instanceof ItemStack) {
+        for (Object x : player.inventoryContainer.getInventory()) {
+            if (x instanceof ItemStack) {
                 ItemStack item = (ItemStack) x;
-                if(item.getItem().getUnlocalizedName().toLowerCase().contains("gun")) {
-                    player.inventoryContainer.putStackInSlot(player.inventoryContainer.getInventory().indexOf(item), null);
-                    System.out.println("Removed " + item.getDisplayName() + " from " + player.getDisplayName() + "'s inventory");
-                    player.addChatMessage((IChatComponent) new ChatComponentText("Removed " + item.getDisplayName() + " from your inventory"));
+
+                for (String item_name : items_list.getStringList()) {
+                    if (item.getDisplayName().equals(item_name)) {
+                        player.inventoryContainer.putStackInSlot(player.inventoryContainer.getInventory().indexOf(item),
+                                                                 null);
+
+                        System.out.println("Removed " + item.getDisplayName() + " from " + player.getDisplayName() +
+                                           "'s inventory");
+                        player.addChatMessage((IChatComponent) new ChatComponentText(
+                                "Removed " + item.getDisplayName() + " from your inventory"));
+                    }
                 }
 
             }
--- src/main/java/com/mayankpatibandla/removeguns/config/ConfigurationHandler.java
+++ src/main/java/com/mayankpatibandla/removeguns/config/ConfigurationHandler.java
@@ -3,27 +3,80 @@ package com.mayankpatibandla.removeguns.config;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class ConfigurationHandler {
     private static Configuration configuration;
-
-    //    public static boolean updateCheck = true;
+    private static Property items;
+    //        Wooden Gun
+    //        Wooden Bullet
+    //        Stone Gun
+    //        Stone Bullet
+    //        Iron Gun
+    //        Iron Bullet
+    //        Golden Gun
+    //        Golden Bullet
+    //        Diamond Gun
+    //        Diamond Bullet
+    //        Bronze Gun
+    //        Bronze Bullet
+    //        Emerald Gun
+    //        Emerald Bullet
+    //        Machine Gun
+    //        Black Bullet
+    //        Kunai
+    //        Reverse Kunai
+    //        Shuriken
+    //        Giant Shuriken
+    //        Musket
+    //        GunChainsaw
+    private static final String[] DEFAULT_ITEMS = new String[]{
+            "Wooden Gun",
+            "Wooden Bullet",
+            "Stone Gun",
+            "Stone Bullet",
+            "Iron Gun",
+            "Iron Bullet",
+            "Golden Gun",
+            "Golden Bullet",
+            "Diamond Gun",
+            "Diamond Bullet",
+            "Bronze Gun",
+            "Bronze Bullet",
+            "Emerald Gun",
+            "Emerald Bullet",
+            "Machine Gun",
+            "Black Bullet",
+            "Kunai",
+            "Reverse Kunai",
+            "Shuriken",
+            "Giant Shuriken",
+            "Musket",
+            "GunChainsaw"};
 
     public static void init(String configDir) {
+        System.out.println(configDir);
         if (configuration == null) {
             configuration = new Configuration(new File(configDir + "/RemoveGuns.cfg"));
-            loadConfiguration();
+            configuration.load();
+
+             items = configuration.get("Items", "Item Names", DEFAULT_ITEMS,
+                                               "Add the display names of the items you want to remove from the " +
+                                               "game, one per line (case sensitive)");
+
+            System.out.println(Arrays.toString(items.getStringList()));
+
+            saveConfiguration();
         }
     }
 
-    private static void loadConfiguration() {
-        //        updateCheck = configuration.getBoolean("updateCheck", net.minecraftforge.common.config
-        //        .Configuration.CATEGORY_GENERAL, true, "Enable update checker");
-
+    private static void saveConfiguration() {
         if (configuration.hasChanged()) {
             configuration.save();
+            System.out.println("Saved configuration");
         }
     }
 
@@ -31,10 +84,14 @@ public class ConfigurationHandler {
         return configuration;
     }
 
+    public static Property getItems() {
+        return items;
+    }
+
     @SubscribeEvent
     public void onConfigurationChangedEvent(ConfigChangedEvent event) {
         if (event.modID.equalsIgnoreCase("removeguns")) {
-            loadConfiguration();
+            saveConfiguration();
         }
     }
 }
